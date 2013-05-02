79e9bc6211c900dfb4a34377b15b6a4d9e1b1972
Rob Sanchez <rob@robsanchez.com>
2013-05-02 16:24:43 -0500

    add route caching

--- system/expressionengine/third_party/template_routes/ext.template_routes.php
+++ system/expressionengine/third_party/template_routes/ext.template_routes.php
@@ -44,6 +44,13 @@ class Template_routes_ext {
 	{
 		$this->EE =& get_instance();
 		$this->settings = $settings;
+
+		if ( ! $this->cache_path = $this->EE->config->slash_item('cache_path'))
+		{
+			$this->cache_path = APPPATH.'cache/';
+		}
+
+		$this->cache_path .= 'template_routes/';
 	}// ----------------------------------------------------------------------
 	
 	/**
@@ -60,6 +67,15 @@ class Template_routes_ext {
 	{
 		// Setup custom settings in this array.
 		$this->settings = array();
+
+		if ( ! @mkdir($this->cache_path))
+		{
+			show_error('Could not create cache path: '.$this->cache_path);
+		}
+
+		$this->EE->load->helper('file');
+
+		write_file($this->cache_path.'cached_routes', '');
 		
 		$data = array(
 			'class'		=> __CLASS__,
@@ -101,7 +117,7 @@ class Template_routes_ext {
 		$site_pages = $this->EE->config->item('site_pages');
 
 		$site_id = $this->EE->config->item('site_id');
-
+/*
 		// check if this URI is a Pages URI
 		$is_page = isset($site_pages[$site_id]['uris']) ? in_array('/'.$uri_string, $site_pages[$site_id]['uris']) : FALSE;
 
@@ -110,124 +126,185 @@ class Template_routes_ext {
 		{
 			$this->EE->config->_global_vars['route_1'] = $uri_string;
 		}
+*/
+
+		$route = FALSE;
 
 		// ensure that this is not a Pages URI and that we have good routes
-		if ($is_page === FALSE && is_array($routes))
+		if (is_array($routes))//if ($is_page === FALSE && is_array($routes))
 		{
-			// loop through all the defined routes and check if the uri_string is a match
-			foreach($routes as $rule => $template)
+			// get our route cache, so we don't have to go through a bunch of regexes
+			$this->EE->load->helper('file');
+
+			$config_hash = md5(json_encode($routes));
+
+			$cache_file = $this->cache_path.md5($uri_string);
+
+			if (file_exists($this->cache_path.'cached_routes'))
 			{
-				// normalize the rule
-				$rule = rtrim($rule, '/');
+				$cached_routes = file_get_contents($this->cache_path.'cached_routes');
 
-				// does the rule have any wildcards?
-				$wildcard = strpos($rule, ':');
-
-				// build the regex from the rule wildcard(s)
-				if ($wildcard !== FALSE)
+				//routes have changed, clear the cache
+				if ($cached_routes !== $config_hash)
 				{
-					// check for a :page:XX wildcard
-					if (preg_match('/\(?:page:(\d+)\)?/', $rule, $match) && isset($site_pages[$site_id]['uris'][$match[1]]))
-					{
-						$rule = str_replace($match[0], '('.ltrim($site_pages[$site_id]['uris'][$match[1]], '/').')', $rule);
-					}
+					delete_files($this->cache_path, TRUE);
 
-					$regex = str_replace(
-						array(
-							'(:any)',
-							':any',
-							'(:num)',
-							':num',
-							'(:year)',
-							':year',
-							'(:month)',
-							':month',
-							'(:day)',
-							':day',
-							'(:category)',
-							':category',
-							'/(:pagination)',
-							'/:pagination',
-							'(:pagination)',
-							':pagination',
-							'/(:all)',
-							'/:all',
-							':all',
-						),
-						array(
-							'([^/]+)',
-							'([^/]+)',
-							'(\d+)',
-							'(\d+)',
-							'(\d{4})',
-							'(\d{4})',
-							'(\d{2})',
-							'(\d{2})',
-							'(\d{2})',
-							'(\d{2})',
-							preg_quote($this->EE->config->item('reserved_category_word')).'/'.($this->EE->config->item('use_category_name') === 'y' ? '([^/]+)' : '(\d+)'),
-							preg_quote($this->EE->config->item('reserved_category_word')).'/'.($this->EE->config->item('use_category_name') === 'y' ? '([^/]+)' : '(\d+)'),
-							'(/P\d+)?',
-							'(/P\d+)?',
-							'(/P\d+)?',
-							'(/P\d+)?',
-							'/?(.*?)',
-							'.*?',
-							'.*?',
-						),
-						$rule
-					);
-				}
-				else
-				{
-					$regex = $rule;
+					write_file($this->cache_path.'cached_routes', md5(json_encode($routes)));
 				}
 
-				$regex = '#^'.trim($regex, '/').'$#';
-
-				// check if the uri_string matches this route
-				if (preg_match($regex, $uri_string, $matches))
+				if (file_exists($cache_file))
 				{
-					// check if it has wildcards
-					if ($wildcard !== FALSE)
+					if ($cache = @file_get_contents($cache_file))
 					{
-						// the channel module uses this query_string property to do its dynamic stuff
-						// normally gets set in Template::parse_template_uri(), but we are overriding that function here
-						// let's grab the bits of the uri that are dynamic and set that as the query_string
-						// e.g. blog/nested/here/:any => _blog/_view will yield a query_string of that final segment
-						$this->EE->uri->query_string = preg_replace('#^'.preg_quote(substr($rule, 0, $wildcard)).'#', '', $uri_string);
+						$route = @json_decode($cache, TRUE);
 					}
-
-					// I want Structure's global variables set on urls that start with a pages URI
-					// so we tell structure that the uri_string is the first match in the regex
-					if (isset($matches[1]) && isset($this->EE->extensions->OBJ['Structure_ext']) && isset($site_pages[$site_id]['uris']) && in_array('/'.$matches[1], $site_pages[$site_id]['uris']))
-					{
-						$temp_uri_string = $this->EE->uri->uri_string;
-
-						$this->EE->uri->uri_string = $matches[1];
-
-						$this->EE->extensions->OBJ['Structure_ext']->sessions_start($this->EE->session);
-
-						$this->EE->uri->uri_string = $temp_uri_string;
-					}
-
-					// loop through the matched sub-strings
-					foreach ($matches as $i => $match)
-					{
-						// set each sub-string as a global template variable
-						$this->EE->config->_global_vars['route_'.$i] = $match;
-
-						// replace any sub-string matches in the template definition
-						$template = str_replace('$'.$i, $match, $template);
-					}
-
-					// prevent other extensions from messing with us
-					$this->EE->extensions->end_script = TRUE;
-					
-					// set the route as array from the template string
-					return explode('/', $template);
 				}
 			}
+
+			// didn't find a cached route, go through the list of routes
+			if ( ! $route)
+			{
+				$found_match = FALSE;
+
+				// loop through all the defined routes and check if the uri_string is a match
+				foreach($routes as $rule => $template)
+				{
+					// normalize the rule
+					$rule = rtrim($rule, '/');
+
+					// does the rule have any wildcards?
+					$wildcard = strpos($rule, ':');
+
+					// build the regex from the rule wildcard(s)
+					if ($wildcard !== FALSE)
+					{
+						// check for a :page:XX wildcard
+						if (preg_match('/\(?:page:(\d+)\)?/', $rule, $match) && isset($site_pages[$site_id]['uris'][$match[1]]))
+						{
+							$rule = str_replace($match[0], '('.ltrim($site_pages[$site_id]['uris'][$match[1]], '/').')', $rule);
+						}
+
+						$regex = str_replace(
+							array(
+								'(:any)',
+								':any',
+								'(:num)',
+								':num',
+								'(:year)',
+								':year',
+								'(:month)',
+								':month',
+								'(:day)',
+								':day',
+								'(:category)',
+								':category',
+								'/(:pagination)',
+								'/:pagination',
+								'(:pagination)',
+								':pagination',
+								'/(:all)',
+								'/:all',
+								':all',
+							),
+							array(
+								'([^/]+)',
+								'([^/]+)',
+								'(\d+)',
+								'(\d+)',
+								'(\d{4})',
+								'(\d{4})',
+								'(\d{2})',
+								'(\d{2})',
+								'(\d{2})',
+								'(\d{2})',
+								preg_quote($this->EE->config->item('reserved_category_word')).'/'.($this->EE->config->item('use_category_name') === 'y' ? '([^/]+)' : '(\d+)'),
+								preg_quote($this->EE->config->item('reserved_category_word')).'/'.($this->EE->config->item('use_category_name') === 'y' ? '([^/]+)' : '(\d+)'),
+								'(/P\d+)?',
+								'(/P\d+)?',
+								'(/P\d+)?',
+								'(/P\d+)?',
+								'/?(.*?)',
+								'.*?',
+								'.*?',
+							),
+							$rule
+						);
+					}
+					else
+					{
+						$regex = $rule;
+					}
+
+					$regex = '#^'.trim($regex, '/').'$#';
+
+					// check if the uri_string matches this route
+					if (preg_match($regex, $uri_string, $matches))
+					{
+						$found_match = TRUE;
+
+						$route['template'] = $template;
+						$route['matches'] = $matches;
+
+						// check if it has wildcards
+						if ($wildcard !== FALSE)
+						{
+							// the channel module uses this query_string property to do its dynamic stuff
+							// normally gets set in Template::parse_template_uri(), but we are overriding that function here
+							// let's grab the bits of the uri that are dynamic and set that as the query_string
+							// e.g. blog/nested/here/:any => _blog/_view will yield a query_string of that final segment
+							$route['query_string'] = preg_replace('#^'.preg_quote(substr($rule, 0, $wildcard)).'#', '', $uri_string);
+						}
+
+						write_file($cache_file, json_encode($route));
+
+						break;
+					}
+				}
+
+				if ( ! $found_match)
+				{
+					$route = array('template' => '');
+
+					write_file($cache_file, json_encode($route));
+				}
+			}
+		}
+
+		if ( ! empty($route['template']))
+		{
+			if (isset($route['query_string']))
+			{
+				$this->EE->uri->query_string = $route['query_string'];
+			}
+
+			// I want Structure's global variables set on urls that start with a pages URI
+			// so we tell structure that the uri_string is the first match in the regex
+			if (isset($route['matches'][1]) && isset($this->EE->extensions->OBJ['Structure_ext']) && isset($site_pages[$site_id]['uris']) && in_array('/'.$route['matches'][1], $site_pages[$site_id]['uris']))
+			{
+				$temp_uri_string = $this->EE->uri->uri_string;
+
+				$this->EE->uri->uri_string = $route['matches'][1];
+
+				$this->EE->extensions->OBJ['Structure_ext']->sessions_start($this->EE->session);
+
+				$this->EE->uri->uri_string = $temp_uri_string;
+			}
+
+			// loop through the matched sub-strings
+			foreach ($route['matches'] as $i => $match)
+			{
+				// set each sub-string as a global template variable
+				$this->EE->config->_global_vars['route_'.$i] = $match;
+
+				// replace any sub-string matches in the template definition
+				$route['template'] = str_replace('$'.$i, $match, $route['template']);
+			}
+
+			// prevent other extensions from messing with us
+			$this->EE->extensions->end_script = TRUE;
+			
+			// set the route as array from the template string
+			return explode('/', $route['template']);
 		}
 
 		// set the default route to any other extension calling this hook
